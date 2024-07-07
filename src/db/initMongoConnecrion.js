2777f92eb295a57fb607b8dff5cbf595a2b3e19b
Oleksandr-Santalov <santalov@ukr.net>
2024-07-07 14:23:43 +0100

    connect

--- src/db/initMongoConnecrion.js
+++ src/db/initMongoConnecrion.js
@@ -9,7 +9,7 @@ export const initMongoConnection = async () => {
 
   try {
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=MyCluster`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
 
     console.log('Mongo connection successfully established!');
