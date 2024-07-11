8a8307a796c6e22d0cd0ce14e2f8944ef6bd47df
Oleksandr-Santalov <santalov@ukr.net>
2024-07-11 11:41:52 +0100

    nodejshw3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-const init = async () => {
-  await initMongoConnection();
-  setupServer();
-};
-
-init();
